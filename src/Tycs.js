687d79d3d8f4d145c867d51bdf730f0ca655dd8c
anuragsir <mkmpanurag@gmail.com>
2026-06-25 12:38:48 +0530

    git pages

---
+++ src/Tycs.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Tycs() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome TYCS
+        </p>
+          
+      </header>
+    </div>
+  );
+}
+
+export default Tycs;
